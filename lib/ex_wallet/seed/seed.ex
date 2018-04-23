f2ada016922a0f31fdd5f1f490de759916fedca9
Kamil Lelonek <squixy.sln@gmail.com>
2018-04-23 17:28:59 +0200

    Improve naming

--- lib/ex_wallet/seed/seed.ex
+++ lib/ex_wallet/seed/seed.ex
@@ -1,8 +1,8 @@
 defmodule ExWallet.Seed do
-  @iterations_count 2048
-  @initial_index 1
+  @rounds_count 2048
+  @initial_round_number 1
   @seed_length div(512, 8)
-  @initial_acc []
+  @result []
 
   def generate(mnemonic, passphrase \\ "") do
     passphrase
@@ -14,22 +14,22 @@ defmodule ExWallet.Seed do
 
   defp pbkdf2(salt, mnemonic) do
     mnemonic
-    |> hash(salt)
-    |> encode_iterations(salt, mnemonic)
+    |> initial_round(salt)
+    |> encode_rounds(salt, mnemonic)
   end
 
-  defp hash(mnemonic, salt),
-    do: hmac_sha512(mnemonic, <<salt::binary, @initial_index::integer-size(32)>>)
+  defp initial_round(mnemonic, salt),
+    do: hmac_sha512(mnemonic, <<salt::binary, @initial_round_number::integer-size(32)>>)
 
-  defp encode_iterations(initial, salt, mnemonic) do
+  defp encode_rounds(initial_block, salt, mnemonic) do
     salt
-    |> iterate(mnemonic, @initial_index + 1, initial, initial)
+    |> iterate(mnemonic, @initial_round_number + 1, initial_block, initial_block)
     |> encode()
   end
 
   defp encode(acc)
        when not is_list(acc),
-       do: encoode([acc, [acc, @initial_acc]])
+       do: encoode([acc, [acc, @result]])
 
   defp encoode(acc) do
     acc
@@ -40,18 +40,15 @@ defmodule ExWallet.Seed do
 
   defp seed(<<seed::binary-size(@seed_length), _::binary>>), do: Base.encode16(seed, case: :lower)
 
-  defp iterate(_salt, _mnemonic, iteration, _prev, acc)
-       when iteration > @iterations_count,
+  defp iterate(_salt, _mnemonic, round_number, _previous_block, acc)
+       when round_number > @rounds_count,
        do: acc
 
-  defp iterate(salt, mnemonic, iteration, prev, acc) do
-    mnemonic
-    |> hmac_sha512(prev)
-    |> do_iterate(salt, mnemonic, iteration, acc)
+  defp iterate(salt, mnemonic, round_number, previous_block, acc) do
+    with next_block = hmac_sha512(mnemonic, previous_block) do
+      iterate(salt, mnemonic, round_number + 1, next_block, xor(next_block, acc))
+    end
   end
-
-  defp do_iterate(next, salt, mnemonic, iteration, acc),
-    do: iterate(salt, mnemonic, iteration + 1, next, xor(next, acc))
 
   defp hmac_sha512(key, data), do: :crypto.hmac(:sha512, key, data)
 
