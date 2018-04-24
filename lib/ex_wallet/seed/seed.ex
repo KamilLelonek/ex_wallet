5333ddd58c8658a14556209f1fb609dd4bf3f4f0
Kamil Lelonek <squixy.sln@gmail.com>
2018-04-24 14:44:00 +0200

    Simplify Seed generation

--- lib/ex_wallet/seed/seed.ex
+++ lib/ex_wallet/seed/seed.ex
@@ -1,47 +1,35 @@
 defmodule ExWallet.Seed do
   @rounds_count 2048
   @initial_round_number 1
-  @seed_length div(512, 8)
 
   def generate(mnemonic, passphrase \\ "") do
     passphrase
     |> salt()
+    |> initial_round(mnemonic)
     |> pbkdf2(mnemonic)
+    |> encode()
   end
 
   defp salt(passphrase), do: "mnemonic" <> passphrase
 
-  defp pbkdf2(salt, mnemonic) do
-    with initial_block = initial_round(mnemonic, salt) do
-      salt
-      |> iterate(mnemonic, @initial_round_number + 1, initial_block, initial_block)
-      |> seed()
-    end
-  end
-
-  defp initial_round(mnemonic, salt),
+  defp initial_round(salt, mnemonic),
     do: hmac_sha512(mnemonic, <<salt::binary, @initial_round_number::integer-size(32)>>)
 
-  defp seed(acc) do
-    [acc, [acc, []]]
-    |> Enum.reverse()
-    |> IO.iodata_to_binary()
-    |> encode()
-  end
+  defp pbkdf2(initial_block, mnemonic),
+    do: iterate(mnemonic, @initial_round_number + 1, initial_block, initial_block)
 
-  defp encode(<<seed::binary-size(@seed_length), _::binary>>), do: Base.encode16(seed, case: :lower)
-
-  defp iterate(_salt, _mnemonic, round_number, _previous_block, acc)
+  defp iterate(_mnemonic, round_number, _previous_block, result)
        when round_number > @rounds_count,
-       do: acc
+       do: result
 
-  defp iterate(salt, mnemonic, round_number, previous_block, acc) do
-    with next_block = hmac_sha512(mnemonic, previous_block) do
-      iterate(salt, mnemonic, round_number + 1, next_block, xor(next_block, acc))
-    end
+  defp iterate(mnemonic, round_number, previous_block, result) do
+    with next_block = hmac_sha512(mnemonic, previous_block),
+      do: iterate(mnemonic, round_number + 1, next_block, xor(next_block, result))
   end
 
   defp hmac_sha512(key, data), do: :crypto.hmac(:sha512, key, data)
 
   defp xor(a, b), do: :crypto.exor(a, b)
+
+  defp encode(result), do: Base.encode16(result, case: :lower)
 end
