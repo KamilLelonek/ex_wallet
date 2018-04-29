42eb82effe493d221c95f0e172278d65873c995c
Kamil Lelonek <squixy.sln@gmail.com>
2018-04-29 22:41:29 +0200

    Remove serialization from MasterKey

--- lib/ex_wallet/seed/master_key.ex
+++ lib/ex_wallet/seed/master_key.ex
@@ -1,38 +1,21 @@
 defmodule ExWallet.Seed.MasterKey do
-  alias ExWallet.Base58
-
-  defstruct [:private_key, :chain_code, :serial]
+  defstruct [:private_key, :chain_code]
 
   @bitcoin_key "Bitcoin seed"
-  @network_prefixes %{
-    main: <<4, 136, 173, 228>>,
-    test: <<4, 53, 131, 148>>
-  }
 
-  def create(seed, network \\ :main) do
+  def create(seed) do
     seed
     |> Base.decode16!(case: :lower)
     |> hmac_sha512()
-    |> new(network)
+    |> new()
   end
 
   defp hmac_sha512(seed), do: :crypto.hmac(:sha512, @bitcoin_key, seed)
 
-  defp new(<<private_key::binary-32, chain_code::binary-32>>, network) do
+  defp new(<<private_key::binary-32, chain_code::binary-32>>) do
     %__MODULE__{
       chain_code: chain_code,
-      private_key: private_key,
-      serial: serialize(private_key, chain_code, network)
+      private_key: private_key
     }
   end
-
-  defp serialize(private_key, chain_code, network) do
-    @network_prefixes
-    |> Map.get(network)
-    |> prepend_serial(chain_code, private_key)
-    |> Base58.check_encode()
-  end
-
-  defp prepend_serial(network_prefix, chain_code, private_key),
-    do: <<network_prefix::binary, 0::72, chain_code::binary, 0::8, private_key::binary>>
 end
