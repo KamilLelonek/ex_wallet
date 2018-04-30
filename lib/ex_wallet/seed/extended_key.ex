3ea78615fa13c3c4e98abacb66f75e4bec691d91
Kamil Lelonek <squixy.sln@gmail.com>
2018-04-30 17:42:07 +0200

    Fix naming

--- lib/ex_wallet/seed/extended_key.ex
+++ lib/ex_wallet/seed/extended_key.ex
@@ -20,7 +20,7 @@ defmodule ExWallet.Seed.ExtendedKey do
 
   def private(private_key, chain_code, network) do
     private_key
-    |> prepend_zeros()
+    |> prepend_index()
     |> serialize(chain_code, :private, network)
   end
 
@@ -33,7 +33,7 @@ defmodule ExWallet.Seed.ExtendedKey do
   defp compress(0, x_coordinate), do: <<0x02::8, x_coordinate::256>>
   defp compress(1, x_coordinate), do: <<0x03::8, x_coordinate::256>>
 
-  defp prepend_zeros(private_key), do: <<0::8, private_key::binary>>
+  defp prepend_index(private_key), do: <<0::8, private_key::binary>>
 
   defp serialize(key, chain_code, type, network) do
     @version_numbers
@@ -43,6 +43,6 @@ defmodule ExWallet.Seed.ExtendedKey do
     |> Base58.check_encode()
   end
 
-  defp prepend_serial(varsion_number, chain_code, key),
-    do: <<varsion_number::binary, 0::72, chain_code::binary, key::binary>>
+  defp prepend_serial(version_number, chain_code, key),
+    do: <<version_number::binary, 0::72, chain_code::binary, key::binary>>
 end
