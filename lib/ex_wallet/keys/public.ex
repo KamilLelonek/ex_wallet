4a1ccf5267e505048eab9831df7195c7607f9ecf
Kamil Lelonek <squixy.sln@gmail.com>
2018-06-29 23:38:05 +0200

    Provide a PublicKey struct

---
+++ lib/ex_wallet/keys/public.ex
@@ -0,0 +1,33 @@
+defmodule ExWallet.Keys.Public do
+  defstruct network: :main,
+            version_number: nil,
+            key: nil,
+            chain_code: <<0>>,
+            depth: 0,
+            fingerprint: <<0::32>>,
+            child_number: 0
+
+  @version_numbers %{
+    main: <<4, 136, 178, 30>>,
+    test: <<4, 53, 135, 207>>
+  }
+
+  def new(
+        network,
+        key,
+        chain_code \\ <<0>>,
+        depth \\ 0,
+        fingerprint \\ <<0::32>>,
+        child_number \\ 0
+      ) do
+    %__MODULE__{
+      network: network,
+      version_number: @version_numbers[network],
+      key: key,
+      chain_code: chain_code,
+      depth: depth,
+      fingerprint: fingerprint,
+      child_number: child_number
+    }
+  end
+end
---
+++ test/ex_wallet/keys/public_test.exs
@@ -0,0 +1,86 @@
+defmodule ExWallet.Keys.PublicTest do
+  use ExUnit.Case, async: true
+
+  alias ExWallet.Keys.Public
+
+  @chain_code <<0>>
+  @child_number 0
+  @depth 0
+  @fingerprint <<0, 0, 0, 0>>
+  @key <<0>>
+  @network :main
+
+  describe "new" do
+    test "should have the default values" do
+      assert %Public{
+               chain_code: @chain_code,
+               child_number: @child_number,
+               depth: @depth,
+               fingerprint: @fingerprint,
+               key: nil,
+               network: :main,
+               version_number: nil
+             } = %Public{}
+    end
+
+    test "should provide the default values for the main network" do
+      network = :main
+
+      assert %Public{
+               chain_code: @chain_code,
+               child_number: @child_number,
+               depth: @depth,
+               fingerprint: @fingerprint,
+               key: @key,
+               network: ^network,
+               version_number: <<4, 136, 178, 30>>
+             } = Public.new(network, @key)
+    end
+
+    test "should provide the default values for the test network" do
+      network = :test
+
+      assert %Public{
+               chain_code: @chain_code,
+               child_number: @child_number,
+               depth: @depth,
+               fingerprint: @fingerprint,
+               key: @key,
+               network: ^network,
+               version_number: <<4, 53, 135, 207>>
+             } = Public.new(network, @key)
+    end
+
+    test "should fill the given values" do
+      assert %Public{
+               chain_code: @chain_code,
+               child_number: @child_number,
+               depth: @depth,
+               fingerprint: @fingerprint,
+               key: @key,
+               network: @network,
+               version_number: <<4, 136, 178, 30>>
+             } =
+               Public.new(
+                 @network,
+                 @key,
+                 @chain_code,
+                 @depth,
+                 @fingerprint,
+                 @child_number
+               )
+    end
+
+    test "should fill the given values with an invalid network" do
+      assert %Public{
+               chain_code: @chain_code,
+               child_number: @child_number,
+               depth: @depth,
+               fingerprint: @fingerprint,
+               key: @key,
+               network: :x,
+               version_number: nil
+             } = Public.new(:x, @key)
+    end
+  end
+end
